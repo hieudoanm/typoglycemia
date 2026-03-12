cbb399e1212461b809d5030f242d13ab33e32bcd
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-13 00:07:24 +0700

    update data-theme

--- packages/app/src/pages/_document.tsx
+++ packages/app/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 const Document: FC = () => {
   return (
-    <Html lang="en" data-theme="black">
+    <Html lang="en" data-theme="luxury">
       <Head />
       <body className="antialiased">
         <Main />
