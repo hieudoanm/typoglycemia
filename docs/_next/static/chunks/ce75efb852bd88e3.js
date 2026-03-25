02898235f132ca8b8daea112a5dd7c8a8b5652ed
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-25 20:52:44 +0700

    Add Head Template

--- docs/_next/static/chunks/ce75efb852bd88e3.js
+++ docs/_next/static/chunks/ce75efb852bd88e3.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/ae506296dc026a9e.js",
   "static/chunks/edcacde1f8fe5d43.js",
   "static/chunks/2372652096593f98.js",
-  "static/chunks/e319808c11054f0b.css",
+  "static/chunks/45cab4a4724714b7.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-a90fb7be35d425eb.js"
+  "static/chunks/turbopack-236a68835d8c905e.js"
 ])
